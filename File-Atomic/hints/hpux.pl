d8de6b49d37180115bf00c490eda72a21d49c761
Gisle Aas <gisle@ActiveState.com>
2005-05-13 01:27:04 -0800

    Camel: The "+e boot_ActiveState__File__Atomic" does not make gcc happy. Ref http://bugs.activestate.com/show_bug.cgi?id=38840
    [git-p4: depot-paths = "//depot/main/Camel/src/as-mod/ActiveState-Utils/main/": change = 143628]

--- File-Atomic/hints/hpux.pl
+++ File-Atomic/hints/hpux.pl
@@ -2,5 +2,5 @@
 # need to make sure that only the XS boot function is exported to avoid
 # symbol issues wiht the HP loader.
 $self->{dynamic_lib} = { 
-    OTHERLDFLAGS => '+e boot_ActiveState__File__Atomic' 
-};
+    OTHERLDFLAGS => '+e boot_ActiveState__File__Atomic'
+} if $Config{ld} ne "gcc";
