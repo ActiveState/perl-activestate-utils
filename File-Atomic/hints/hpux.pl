110cf0c8029b6e650f50832f0a45ff925a53ea60
Philippe M. Chiasson <gozer@activestate.com>
2005-07-06 12:33:44 -0800

    Camel ActiveState-Utils-Atomic failed to pass the correct options to the linker    when it's cc. Bug 39702
    [git-p4: depot-paths = "//depot/main/Camel/src/as-mod/ActiveState-Utils/main/": change = 157315]

--- File-Atomic/hints/hpux.pl
+++ File-Atomic/hints/hpux.pl
@@ -3,4 +3,9 @@
 # symbol issues wiht the HP loader.
 $self->{dynamic_lib} = { 
     OTHERLDFLAGS => '+e boot_ActiveState__File__Atomic'
-} if $Config{ld} ne "gcc";
+} if $Config{ld} eq "ld";
+
+$self->{dynamic_lib} = {
+    OTHERLDFLAGS => '-Bextern=boot_ActiveState__File__Atomic'
+} if $Config{ld} eq "cc";
+
